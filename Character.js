f3990a72acc04df9818cb5cad19e28eb086130c8
beapascual <btp6195@nyu.edu>
2023-12-01 18:14:06 -0500

    created class for character, made it constantly moving

---
+++ Character.js
@@ -0,0 +1,24 @@
+class Character{
+
+    constructor(x,y){
+        this.pos = createVector(50,height-25);
+        this.vel = createVector(1,0);
+        this.acc = createVector(0,0);
+
+    }
+
+    applyForce(force){
+        this.acc.add(force)
+    }
+
+    update(){
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.set(0,0);
+    }
+    
+    display(){
+        fill(255)
+        ellipse(this.pos.x,this.pos.y,50)
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 function setup() {
   createCanvas(800, 800);
+  character = new Character;
+
 }
 
 function draw() {
   // put drawing code here
   background(0);
+  character.update();
+  character.display();
+
+
 }
