56e2af6fec24c8f080fa8eada83ffdae163cc39e
beapascual <btp6195@nyu.edu>
2023-12-06 21:06:34 -0500

    tested out different sprites (failed)

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-let ball;
+let ball, floor;
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(238, 200);
+	world.gravity.y = 10;
 
 	ball = new Sprite();
 	ball.diameter = 50;
-}
+	ball.y = 30;
 
-function draw() {
-	background('gray');
+	floor = new Sprite();
+	floor.y = 190;
+	floor.w = 238;
+	floor.h = 5;
+	//floor.collider = 'static';
 }
