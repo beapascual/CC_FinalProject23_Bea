f4b71b61107df511eda687bfe986c08c98587f0f
beapascual <btp6195@nyu.edu>
2023-12-07 21:58:09 -0500

    making sure normal scene changing works (want to apply it to button)

--- sketch.js
+++ sketch.js
@@ -9,21 +9,13 @@ function setup() {
   character = new Character();
   option = 0
 
-  button = createButton("Ask 8Ball"); // creates button that says "Ask 8Ball"
-  button.position (330,130); // position where button is
-  button.style ('font-size', '30px'); // size of letters in button
-  button.mousePressed(level1);
+  // button = createButton("Ask 8Ball"); // creates button that says "Ask 8Ball"
+  // button.position (330,130); // position where button is
+  // button.style ('font-size', '30px'); // size of letters in button
+  // button.mousePressed(level1);
 
 
-  let gravity = createVector(0,0.1);
-  character.applyForce (gravity);
-
-
-
-  translate (-character.pos.x+50,0)
-  character.update();
-  character.checkEdges();
-  character.display();
+ 
 
 }
 
@@ -31,6 +23,9 @@ function keyPressed(){
   if (key === ' '){
     let jump = createVector(0, -5 );
     character.applyForce(jump);
+  }
+  if (key === 'l'){
+    option ++
   }
 }
 
@@ -41,9 +36,16 @@ function keyPressed(){
 
 
 
-function level1(){
+function draw(){
 
-  loop()
+if (option === 0){
+  background (100,0,100); 
+}
+
+if (option === 1){
+
+  background(0);
+
   let gravity = createVector(0,0.1);
   character.applyForce (gravity);
 
@@ -67,5 +69,6 @@ fill (127)
 rect (0,400,width*6,height)
 rect (0,0,width*6,200)
 
+}
 }
  
