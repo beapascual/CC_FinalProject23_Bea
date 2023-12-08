a5f4de0ac2cc06ae083f6afa710e2f132c5d1736
beapascual <btp6195@nyu.edu>
2023-12-07 21:51:44 -0500

    attempt to make level button work (still failing)

--- sketch.js
+++ sketch.js
@@ -7,26 +7,14 @@ let option;
 function setup() {
   createCanvas(800, 600);
   character = new Character();
-  
+  option = 0
+
   button = createButton("Ask 8Ball"); // creates button that says "Ask 8Ball"
   button.position (330,130); // position where button is
   button.style ('font-size', '30px'); // size of letters in button
-  button.mousePressed(option = 1)
+  button.mousePressed(level1);
 
-}
-
-function keyPressed(){
-  if (key === ' '){
-    let jump = createVector(0, -5 );
-    character.applyForce(jump);
-  }
-}
-
-function draw() {
-  // put drawing code here
-  if (option === 1){
-
-  
+  loop()
   background(0);
 
   let gravity = createVector(0,0.1);
@@ -38,7 +26,26 @@ function draw() {
   character.update();
   character.checkEdges();
   character.display();
-  
+
+}
+
+function keyPressed(){
+  if (key === ' '){
+    let jump = createVector(0, -5 );
+    character.applyForce(jump);
+  }
+}
+
+function draw () {
+    background (0);
+}
+ 
+
+
+
+function level1(){
+
+
 for(let x = 0; x < width*5; x = x + 300){
   fill (0,255,0);
   triangle(x + 400,400,x+450,400,x+425,400-50);
@@ -53,4 +60,4 @@ rect (0,400,width*6,height)
 rect (0,0,width*6,200)
 
 }
-}
+ 
