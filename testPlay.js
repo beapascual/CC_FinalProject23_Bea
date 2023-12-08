bfe10a241a6456c163c106d8aaa7539e005be13c
beapascual <btp6195@nyu.edu>
2023-12-08 17:01:05 -0500

    got p5 play working again!!

--- testPlay.js
+++ testPlay.js
@@ -1,21 +1,20 @@
 let ball, floor;
 
 function setup() {
-	new Canvas(400, 400);
+	new Canvas(238, 200);
 	world.gravity.y = 10;
-	
-	ball = new Sprite();
-	ball.diameter = 50
-	player = new Sprite(30, 0, 50);
 
-	floor1 = new Sprite();
-	floor1.y = 300
-	floor1.w = 250
-	floor1.h = 5
-	floor1.collider = 'static'
+	ball = new Sprite();
+	ball.diameter = 50;
+	ball.y = 30;
+
+	floor = new Sprite();
+	floor.y = 190;
+	floor.w = 238;
+	floor.h = 5;
+	floor.collider = 'static';
 }
 
 function draw() {
 	clear();
-
 }
