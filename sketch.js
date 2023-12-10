abff4220423b10ded60dd031189ba7d17d8dca4d
beapascual <btp6195@nyu.edu>
2023-12-10 14:49:28 -0500

    got sprites to disappear when lives = 0

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ function setup() {
 	player.y = 200;
   player.img = 'images/bee.png'
   player.scale = 0.05
+  player.vel.x = 4
   // player.rotationLock = false
 	// ball.vel.x = 4
   
@@ -24,7 +25,7 @@ clouds = new Group ();
 for (let j = 1; j < 6; j++){
   let cloud = new Sprite(400 * j, 250)
   cloud.img = 'images/cloud.png'
-  cloud.collider = 'static'
+  cloud.collider = 'kinematic'
   cloud.scale = (0.1)
   cloud.w = 125
   cloud.h = 50
@@ -65,33 +66,36 @@ textSize(20);
 function draw() {
 
 
- 
+if (lives > 0){
 	translate (-player.x + 50, 0)
 	clear();
-  if(player.x < 2400){
-    player.vel.x = 4
-  } else{
-    player.vel.x = 0
-  }
   background(0,0,100)
 
 
 
-player.collides(clouds, stop);
+
+  player.collides(clouds, stop);
+
+
+  if (player.y > height){
+  stop();
+  }
 }
 
-// if (player.y > height){
-//   stop();
-// }
+if (lives === 0){
+  clearSprites();
+  // background (100,0,0);
+  // text ("YOU LOSE", windowWidth/2, windowHeight/2);
+}
  
 
-// text ("LIVES: " + lives, player.x, 20);
-// text ("COINS: " + coins, player.x, 50);
 
 
+text ("LIVES: " + lives, player.x, 20);
+text ("COINS: " + coins, player.x, 50);
 
 
-// }
+}
 
 function keyPressed(){
 
@@ -107,4 +111,9 @@ function stop(){
   player. y = 200
   player.vel.x = 0
   lives --
+}
+
+function clearSprites(){
+  clouds.removeSprites();
+  player.remove();
 }
