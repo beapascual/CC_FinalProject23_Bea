896d88a5a23620623acca10621b08f9b55573865
beapascual <btp6195@nyu.edu>
2023-12-11 02:01:26 -0500

    cleaned up code, tried to add level text, failed

--- sketch.js
+++ sketch.js
@@ -2,22 +2,33 @@ let player, floor, cloud;
 let lives = 3;
 let coins = 0;
 let level = 1
+let img1, img2;
+
+
+function preload(){
+  img1 = loadImage('images/bee.png')
+  img2 = loadImage('images/cloud.png')
+}
 
 
 function setup() {
 	new Canvas(3000, 400);
 	world.gravity.y = 10;
-  textSize(20);
-
+  textSize(20); // world basics
 
 	player = new Sprite();
 	player.x = 0
 	player.y = 200;
-  player.img = 'images/bee.png'
-  player.scale = 0.05
+  player.img = img1
+  player.scale = 0.05 //player settings
 
-  
+  clouds = new Group();
+  clouds.img = img2;
+  clouds.scale = 0.1
+  clouds.collider = 'kinematic' //clouds settings
 
+  g1 = color(232, 86, 94);
+  g2 = color(232, 170, 69);// colors for gradient
 
 
 
@@ -31,24 +42,7 @@ function setup() {
   //     dot.x = dots.length * 100 + 300;
   //     dot.diameter = 10
   //   }
-
-    // player.collides(dots, collect)
-
-
-  clouds = new Group();
-    clouds.img = 'images/cloud.png';
-    clouds.y = 200;
-    clouds.scale = 0.1
-    clouds.collider = 'kinematic'
-    
-    while (clouds.length < 5) {
-      let cloud = new clouds.Sprite();
-      cloud.x = clouds.length * 400 + 500;
-      cloud.w = 125;
-      cloud.h = 50
-    }
-  
-   
+  // player.collides(dots, collect)
 
 }
 
@@ -57,43 +51,74 @@ function setup() {
 //   coins++
 // }
 
+function level1(){
+  while (clouds.length < 5) {
+    let cloud = new clouds.Sprite();
+    cloud.x = clouds.length * 400 + 500;
+    cloud.y = 200
+    cloud.w = 125;
+    cloud.h = 50
+  }
+}//cloud pattern for level1
+
+
+function setGradient(g1, g2) {
+  noFill();
+  for (var y = 0; y < height; y++) { // as y increases inter increases, creating a smooth gradient
+    var inter = map(y, 0, 300, 0, 1); // from y=0 to y=300, color will change completely from g1 to g2
+    var s1 = lerpColor(g1, g2, inter); // lerp color makes color change from 1 to 2, inter makes smooth gradient
+    stroke(s1); // each line is a different color to make gradient ^^
+    line(0, y, width, y); // makes the color appear
+  }
+} // gradient for level 2 (taken from midterm)
+
 
 function draw() {
-
- 
-
   if (level === 1){
-  if (lives > 0){
-	translate (-player.x + 50, 0)
-	clear();
-  background(0,0,100)
-
-
- if (player.x <= 2800){
-  player.vel.x = 4
- } else if (player.x > 2800) {
-  level = 2
- }
+    level1();
+    fill (255)
+    stroke (0)
+    strokeWeight(2)
+    text ("LEVEL 1", player.x + 500, 50);
+    if (lives > 0){
+	    translate (-player.x + 50, 0)
+	    clear();
+      background(115,239,245)
+    if (player.x > 2800) {
+      level = 2
+      }
+    }
   }
 
-  }
-if (level === 2){
-  if (lives > 0){
+  if (level === 2){
+    player.x = 0
+    if (lives > 0){
     translate (-player.x + 50, 0)
     clear();
-    background(0)
-  
-  
-   if (player.x <= 2800){
-    player.vel.x = 4
-   } else if (player.x > 2800) {
-    level = 2
-   }
+    setGradient(g1,g2);
+    if (player.x > 2800) {
+      level = 3
+      }
     }
-  // clearSprites();
-}
+  }
 
-  // player.overlaps(dots, collect); 
+  if (level === 3){
+    player.x = 0
+    if (lives > 0){
+    translate (-player.x + 50, 0)
+    clear();
+    background(0,0,100)
+    if (player.x > 2800) {
+    level = 4
+      }
+    }
+  }
+
+  if (level === 4){
+    background (255);
+    clearSprites();
+  }
+
 
   if (player.collides(clouds)){
     player.x = 0
@@ -101,11 +126,15 @@ if (level === 2){
     coins = 0
   }
 
-
   if (player.y > height){
   player.x = 0
   player.y = 200
   lives --
+  }
+  if (player.y < 0){
+    player.x = 0
+   player.y = 200
+    lives --
   }
 
   // if (coins = 10){
@@ -113,16 +142,16 @@ if (level === 2){
   //   coins = 0
   // }
 
-
-if (lives === 0){
-  clearSprites();
-}
- 
+  if (lives === 0){
+    clearSprites();
+  }
 
 
-fill(255)
-text ("LIVES: " + lives, player.x, 50);
-text ("COINS: " + coins, player.x, 75);
+  fill(255)
+  stroke(0)
+  strokeWeight(2)
+  text ("LIVES: " + lives, player.x, 50);
+// text ("COINS: " + coins, player.x, 75);
 
 }
 
@@ -132,6 +161,7 @@ text ("COINS: " + coins, player.x, 75);
 function keyPressed(){
 if (key === ' '){
     player.vel.y = -5
+    player.vel.x = 4
   }
 if (key === 'l'){
   level ++
